0fed95440b649ba5337751358b84edee8b4115c7
Svetlana Nosatenko <s.v.nosatenko@gmail.com>
2021-10-17 17:13:57 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from functions import *
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/book/<int:id>')
+def book(id: int):
+    book_found = data_id(id)
+    if data_id(id):
+        return jsonify(book_found), 200
+    return jsonify({"status": "failed"}), 400
+
+
+@app.route('/new/', methods=['POST'])
+def new_book():
+    new_book = request.json
+    new_id = create_id(book)
+    new_book["id"] = new_id
+    isbn = create_isbn()
+    new_book['isbn'] = isbn
+    if add_data(new_book):
+        return jsonify({"status": "success"}), 201
+    return jsonify({"status": "failed"}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ functions.py
@@ -0,0 +1,55 @@
+import json
+import random
+from random import randint
+
+
+def save_data(book):
+    with open('books.json', "w", encoding='utf-8') as f:
+        json.dump(book, f)
+    return True
+
+
+def get_data():
+    with open('books.json', encoding='utf-8') as f:
+        book = json.load(f)
+        return book
+
+
+def add_data(new_book):
+    old_book = get_data()
+    old_book.append(new_book)
+    save_data(old_book)
+    return True
+
+
+def create_id(books):
+    books = get_data()
+    if not books:
+        return 1
+    max_id = 0
+    for book in books:
+        if book['id'] > max_id:
+            max_id = book['id']
+    return max_id + 1
+
+
+def create_isbn() -> str:
+    def gen_digits(count) -> str:
+        return ''.join(map(lambda x: str(randint(0, 9)), range(count)))
+
+    return '{}-{}-{}-{}-{}'.format(
+        gen_digits(3),
+        randint(1, 7),
+        gen_digits(3),
+        '0' + gen_digits(4),
+        randint(1, 4)
+    )
+
+
+def data_id(id):
+    books = get_data()
+    book_found = []
+    for book in books:
+        if book['id'] == id:
+            book_found.append(book)
+    return book_found
